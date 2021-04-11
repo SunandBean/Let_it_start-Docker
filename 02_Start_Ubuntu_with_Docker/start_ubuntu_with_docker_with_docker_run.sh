f4dfdb60f27f359f75f5c4fb31e92878e3eca692
sunandbean <sunbin.vincent.kim@gmail.com>
2021-04-11 20:50:42 +0900

    Upload two scripts for installing ubuntu 18.04 container

---
+++ 02_Start_Ubuntu_with_Docker/start_ubuntu_with_docker_with_docker_run.sh
@@ -0,0 +1,1 @@
+docker run -it --name ubuntu_18_04 ubuntu:18.04 
---
+++ 02_Start_Ubuntu_with_Docker/start_ubuntu_with_docker_without_docker_run.sh
@@ -0,0 +1,8 @@
+docker pull ubuntu:18.04 
+
+docker create -i -t --name ubuntu_18_04 ubuntu:18.04 
+
+docker start ubuntu_18_04 
+
+docker attach ubuntu_18_04 
+
